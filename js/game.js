a8d9074fb407d96d86e84a06bbf388eee3f3622e
Joyce Cheung <joice@Joyces-MacBook-Pro.local>
2016-06-10 10:45:13 +0800

    finished

--- js/game.js
+++ js/game.js
@@ -101,9 +101,9 @@ var Game = function(){
     dimension++;
 
     if (dimension % 2 === 1) {
-      $('.game').css('background-image','url("/Users/joice/flappy/assets/forestbackground.png")');
+      $('.game').css('background-image','url("./assets/forestbackground.png")');
     } else if (dimension % 2 === 0) {
-      $('.game').css('background-image','url("/Users/joice/flappy/assets/mountainbackground.png")');
+      $('.game').css('background-image','url("./assets/mountainbackground.png")');
     }
 
     $('.food').stop().remove()
